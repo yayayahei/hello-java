7f5600779e068c141d250f3e758e6071d7bef3f2
yayayahei <mmwcbz@msn.cn>
2019-08-27 11:46:30 +0800

    add ReadTimeZone class

---
+++ src/com/yayayahei/ReadTimeZone.java
@@ -0,0 +1,13 @@
+package com.yayayahei;
+
+import java.util.TimeZone;
+
+public class ReadTimeZone {
+    public static void main(String[] args) {
+        String[] availableIDs = TimeZone.getAvailableIDs();
+        for (String id : availableIDs) {
+            TimeZone timeZone = TimeZone.getTimeZone(id);
+            System.out.println(timeZone.getID()+": " + timeZone.getDisplayName());
+        }
+    }
+}
