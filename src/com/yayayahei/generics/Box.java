30ed0256ddc1cdfc5c4fc001205c1688407c042e
yayayahei <mmwcbz@msn.cn>
2020-03-18 13:39:17 +0800

    add generic type

---
+++ src/com/yayayahei/generics/Box.java
@@ -0,0 +1,29 @@
+package com.yayayahei.generics;
+
+public class Box<T> {
+    private T t;
+
+    public void set(T t) {
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }
+
+    public <U extends Number> void inspect(U u) {
+        System.out.println("T: " + t.getClass());
+        System.out.println("U: " + u.getClass());
+    }
+
+    protected Class<Long> dataType() {
+        return Long.class;
+    }
+
+    public static void main(String[] args) {
+        Box<Integer> integerBox = new Box<Integer>();
+        integerBox.set(new Integer(10));
+        integerBox.inspect(10l); // error: this is still String!
+        System.out.println("dataType: " + integerBox.dataType());
+    }
+}
