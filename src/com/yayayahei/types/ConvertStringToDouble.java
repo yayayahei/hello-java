a67a82dac9d75748bd9c765c4d99f41fc826e54f
yayayahei <mmwcbz@msn.cn>
2020-04-13 17:28:54 +0800

    add ConvertStringToDouble

---
+++ src/com/yayayahei/types/ConvertStringToDouble.java
@@ -0,0 +1,12 @@
+package com.yayayahei.types;
+
+
+public class ConvertStringToDouble {
+
+    public static void main(String[] args) {
+        String price = "1,549";
+        // this will throw java.lang.NumberFormatException
+        Double priceInDouble = Double.valueOf(price);
+        System.out.println(priceInDouble);
+    }
+}
