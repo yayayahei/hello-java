385f4381db50e87d7d117aa7836bb59841a69bcd
yayayahei <mmwcbz@msn.cn>
2020-04-17 19:45:35 +0800

    add ActuatorSecurity

---
+++ src/main/java/com/yayayahei/hello/spring/actuator/secure/ActuatorSecurity.java
@@ -0,0 +1,17 @@
+package com.yayayahei.hello.spring.actuator.secure;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration(proxyBeanMethods = false)
+public class ActuatorSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requestMatcher(EndpointRequest.toAnyEndpoint()).authorizeRequests((requests) ->
+                requests.anyRequest().hasRole("ENDPOINT_ADMIN"));
+        http.httpBasic();
+    }
+}
